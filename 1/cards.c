e059d2f74944e85536c876b398e981bf1fb73eeb
Shpere Jacinda <sjacinda@va-f4.kzn.21-school.ru>
2021-12-04 20:45:52 +0300

    1

---
+++ 1/cards.c
@@ -0,0 +1,28 @@
+// Программа для оценивания карт.
+// Выпущенно под публичной Лос-Вегасской лицензией.
+// (с) 2021 Команда колледжа по блек-джеку.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char	card_name[3];
+	int		val;
+
+	puts("Введите название карты: ");
+	scanf("%2s", card_name);
+	val = 0;
+	if (card_name[0] == 'K')
+		val = 10;
+	else if (card_name[0] == 'Q')
+		val = 10;
+	else if (card_name[0] == 'J')
+		val = 10;
+	else if (card_name[0] == 'A')
+		val = 11;
+	else
+		val = atoi(card_name);
+	printf("Ценность карты: %i\n", val);
+	return (0);
+}
